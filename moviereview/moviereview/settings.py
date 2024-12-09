5122eeae542161cf49d459f61da6460a06ce8f0d
sosina14 <sosiayu14@gmail.com>
2024-12-09 09:02:57 +0300

    updated

--- moviereview/moviereview/settings.py
+++ moviereview/moviereview/settings.py
@@ -40,6 +40,18 @@ INSTALLED_APPS = [
     'rest_framework',
     'movies',
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- moviereview/moviereview/urls.py
+++ moviereview/moviereview/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import path, include
 from django.urls import path
+from views import MovieListCreateAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('movies.urls')),
+    path('api/movies/', MovieListCreateAPIView.as_view(), name='movie_list_create'),
 ]
